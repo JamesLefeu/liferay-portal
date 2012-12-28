54a68f6e33663c2d357dbe2558216b5073a128b2
James Lefeu <James.Lefeu@liferay.com>
2012-12-27 18:10:17 -0600

    LPS-30153 Searching by No ResourcePermissions

--- portal-service/src/com/liferay/portal/service/LayoutLocalService.java
+++ portal-service/src/com/liferay/portal/service/LayoutLocalService.java
@@ -815,6 +815,17 @@ public interface LayoutLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	/**
+	* Returns all the layouts without resource permissions
+	*
+	* @return all the layouts without resource permissions
+	* @throws SystemException if a system exception occurred
+	*/
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.Layout> getNoPermissionLayouts(
+		com.liferay.portal.model.Role role)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	/**
 	* Returns all the layouts whose friendly URLs are <code>null</code>
 	*
 	* @return all the layouts whose friendly URLs are <code>null</code>
@@ -822,16 +833,6 @@ public interface LayoutLocalService extends BaseLocalService,
 	*/
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public java.util.List<com.liferay.portal.model.Layout> getNullFriendlyURLLayouts()
-		throws com.liferay.portal.kernel.exception.SystemException;
-
-	/**
-	* Returns all the layouts whose UUIDs are <code>null</code>
-	*
-	* @return all the layouts whose UUIDs are <code>null</code>
-	* @throws SystemException if a system exception occurred
-	*/
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public java.util.List<com.liferay.portal.model.Layout> getNullUuidLayouts()
 		throws com.liferay.portal.kernel.exception.SystemException;
 
 	/**
